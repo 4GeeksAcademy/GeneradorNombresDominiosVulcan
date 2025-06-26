1dd9337cc712a79e3c6a01fdfe327e6b8eeca72a
Salvador Gómez  Moya <salvadorgomezmoya@gmail.com>
2025-06-26 22:51:25 +0000

    loop anidado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
 
+  let pron = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let domain = ['.com', '.es'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let p = 0; p < pron.length; p++) {
+    for (let a = 0; a< adj.length; a++) {
+      for (let s = 0; s < noun.length; s++) {
+        for (let d = 0; d < domain.length; d++) {
+          let dom = pron[p] + adj[a] + noun[s] + domain[d];
+          console.log(dom)
+          }
+        }
+      }
+    }
+  
